43e69625c3987ef74c25624007d19b535db6f1f7
chuongminh32 <23110187@student.hcmute.edu.vn>
2024-10-20 01:35:11 +0700

    add audio auto

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ onload = () => {
       clearTimeout(c);
     }, 1000);
   };
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    // Truy cập thẻ audio
+    var audio = document.getElementById("birthDayaudio");
+
+    // Bắt đầu chơi nhạc khi trang web tải xong
+    audio.play();
+});
+document.addEventListener("click", function () {
+    var audio = document.getElementById("birthDayaudio");
+    audio.play();
+});
+document.addEventListener(
+    "click",
+    () => {
+        const AudioTag = document.querySelector("#audio__letter");
+        if (AudioTag) {
+            AudioTag.play();                                        
+        }
+    },
+    { once: true }
+);
